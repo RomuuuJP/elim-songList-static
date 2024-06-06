03886b39f223f7da48d29d57af4c8ab2dcab9e7a
Romuuu <m18660755781@gmail.com>
2024-06-06 17:14:13 +0900

    uploaded

---
+++ js/index-script.js
@@ -0,0 +1,50 @@
+const pages = ['singer/haiyy.html', 'singer/test.html']; // Add your page URLs here
+let currentPageIndex;
+let touchstartX = 0;
+let touchendX = 0;
+
+function getRandomPage() {
+  currentPageIndex = Math.floor(Math.random() * pages.length);
+  return pages[currentPageIndex];
+}
+
+function prevPage() {
+  currentPageIndex = (currentPageIndex === 0) ? pages.length - 1 : currentPageIndex - 1;
+  document.getElementById('iframe-container').src = pages[currentPageIndex];
+}
+
+function nextPage() {
+  currentPageIndex = (currentPageIndex === pages.length - 1) ? 0 : currentPageIndex + 1;
+  document.getElementById('iframe-container').src = pages[currentPageIndex];
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    prevPage();
+  } else if (event.key === 'ArrowRight') {
+    nextPage();
+  }
+});
+
+window.onload = function() {
+  document.getElementById('iframe-container').src = getRandomPage();
+};
+
+document.addEventListener('touchstart', function(event) {
+  touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+document.addEventListener('touchend', function(event) {
+  touchendX = event.changedTouches[0].screenX;
+  handleGesture();
+}, false); 
+
+function handleGesture() {
+  if (touchendX < touchstartX) {
+    nextPage();
+  }
+  
+  if (touchendX > touchstartX) {
+    prevPage();
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,67 @@
+function filterSongs() {
+	var input, filter, ul, li, div, i, txtValue;
+	input = document.getElementById('searchBox');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("songList");
+	li = ul.getElementsByTagName('li');
+
+	for (i = 0; i < li.length; i++) {
+		song = li[i].getElementsByTagName("div")[0];
+		artist = li[i].getElementsByTagName("div")[1];
+		songTxt = song.textContent || song.innerText;
+		artistTxt = artist.textContent || artist.innerText;
+		if (songTxt.toUpperCase().indexOf(filter) > -1 || artistTxt.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
+}
+
+function toggleTheme() {
+	var body = document.body;
+	if (body.getAttribute('data-theme') === 'dark') {
+		body.removeAttribute('data-theme');
+	} else {
+		body.setAttribute('data-theme', 'dark');
+	}
+}
+
+function openInNewTab(url) {
+	var win = window.open(url, '_blank');
+	win.focus();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	var audio = document.getElementById("audio");
+	var playButton = document.getElementById("playButton");
+	var pauseButton = document.getElementById("pauseButton");
+	var stopButton = document.getElementById("stopButton");
+	var progressBar = document.getElementById("progressBar");
+	var progress = document.getElementById("progress");
+
+	playButton.addEventListener("click", function() {
+		audio.play();
+	});
+
+	pauseButton.addEventListener("click", function() {
+		audio.pause();
+	});
+
+	stopButton.addEventListener("click", function() {
+		audio.pause();
+		audio.currentTime = 0;
+	});
+
+	audio.addEventListener("timeupdate", function() {
+		var percentage = (audio.currentTime / audio.duration) * 100;
+		progress.style.width = percentage + "%";
+	});
+
+	progressBar.addEventListener("click", function(event) {
+		var rect = progressBar.getBoundingClientRect();
+		var offsetX = event.clientX - rect.left;
+		var percentage = offsetX / progressBar.offsetWidth;
+		audio.currentTime = percentage * audio.duration;
+	});
+});
